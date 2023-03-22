44aa822412ff27f44cb403c523d92d8d3132ea12
leo velazque <leo velazque@CLINICARDLS.COM>
2023-03-22 15:59:21 -0300

    menu desplegable y js

---
+++ scrypt.js
@@ -0,0 +1,8 @@
+function mostrarListaDesplegable() {
+    var listaDesplegable = document.getElementById("lista-desplegable");
+    if (listaDesplegable.style.display === "none") {
+        listaDesplegable.style.display = "block";
+    } else {
+        listaDesplegable.style.display = "none";
+    }
+}
